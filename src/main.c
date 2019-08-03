7df070cadd37446e8814eff759c7bfb4b855f2db
levy <lboccato@dca.fee.unicamp.br>
2019-08-03 13:47:50 -0300

    Programa funcionando

--- src/main.c
+++ src/main.c
@@ -9,16 +9,26 @@ int main() {
 
   unsigned char entrada;
   unsigned int tmp;
-  unsigned int n_bits;
+  unsigned int n_bits,i = 0;
 
   /* Ler entrada em hexadecimal */
   scanf("%x", &tmp);
-  entrada = (unsigned char)tmp;
+  entrada = (unsigned char) tmp;
 
 
   n_bits = 0;
 
+  /* Contabiliza o numero de bits iguais a 1 presentes na cadeia binaria em entrada (8 bits) */
+  do {
+	
+	n_bits = n_bits + (unsigned int) (entrada & 0x01);
+	entrada = entrada >> 1;
+	i = i + 1;	
+
+  } while (i < 8);
+
   /* Escrever numero de bits */
   printf("%d\n", n_bits);
   return 0;
+
 }
